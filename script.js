c87c1f233d047a03fbf5524cf2190a6233744feb
Lucas Marin <96667789+marinSantos@users.noreply.github.com>
2022-08-25 18:58:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function toggleMenu() {
   nav.classList.toggle('active');
   gray.classList.toggle('acitive');
 }
-
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('toachstart', toggleMenu);
 
 
 //Configuração do menu da opção Features
@@ -15,10 +15,11 @@ const menuA = document.getElementById('mini-menu-1');
 
 
 function miniMenuA() {
+
   menu.classList.toggle('openA');
 }
-
 menuA.addEventListener('click', miniMenuA);
+menuA.addEventListener('toachstart', miniMenuA);
 
 //Configuração do menu da opção Company
 const menuB = document.getElementById('mini-menu-2');
@@ -27,7 +28,5 @@ const menuB = document.getElementById('mini-menu-2');
 function miniMenuB() {
   menu.classList.toggle('openB');
 }
-
 menuB.addEventListener('click', miniMenuB);
-
-
+menuB.addEventListener('toachstart', miniMenuB);
